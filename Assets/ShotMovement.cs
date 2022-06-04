1dd210b0e8c366f691055964cb602f110270e93c
Emmet <emmetgamesinc@gmail.com>
2022-06-04 17:21:54 +0300

    Added handling for the situation of the shots existing for too long

--- Assets/ShotMovement.cs
+++ Assets/ShotMovement.cs
@@ -1,15 +1,24 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class ShotMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float lifetime = 5f;
+
     public float Damage;
 
     private void Awake()
     {
         GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+        StartCoroutine(DestroyAfterTooLong());
+    }
+
+    // Should use pooling instead but damn I don't have time for this
+    IEnumerator DestroyAfterTooLong()
+    {
+        yield return new WaitForSeconds(lifetime);
+        Destroy(gameObject);
     }
 }
