0482cba832dffde9817f622883ad06f9fdde7337
Emmet <emmetgamesinc@gmail.com>
2022-06-05 00:25:44 +0300

    Added sprites and animations for player, enemy, shot

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,13 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 300f;
     [SerializeField] private Rigidbody2D rigidbody;
+    [SerializeField] private Animator animator;
 
     private void Update()
     {
         float xMov = Input.GetAxisRaw("Horizontal");
         rigidbody.velocity = new Vector2(xMov, rigidbody.velocity.y).normalized * speed;
+        animator.SetBool("Moving", Mathf.Abs(xMov) >= 0.05f);
+        animator.GetComponent<SpriteRenderer>().flipX = xMov > 0f;
     }
 }
